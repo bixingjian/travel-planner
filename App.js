acdfdc44b989090f4bca37180b3e1bae436e3871
bixingjian <bixingjian19@163.com>
2023-09-18 19:49:37 -0400

    add welcome screen

--- App.js
+++ App.js
@@ -13,34 +13,8 @@ import {
   StatusBar,
   useWindowDimensions,
 } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  console.log(useWindowDimensions());
-  const { width, height } = useWindowDimensions();
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: width > height ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    // alignItems: "center",
-    // justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  stretch: {
-    width: 200,
-    height: 200,
-    resizeMode: "stretch",
-  },
-});
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-## Publish to Expo
+- publish to expo
+  `eas update`
 
-`eas update`
+- layout
+  - main axis, cross axis. (based on the flex direction)
+  - justifyContent: along main axis. (default col)
+  - alignItems: along seconday axis. each line
+  - align-self: apply to individual item
+  - flexWrap
+  - alignContent: when you have wrapping, align all of the content (multiple lines)
+  - flixBasis(size along the primary axis), flexGrow(the same with flex, grow to fill the avail space), flexShrink(not often, opposite of flexGrow, the same with set flex to -1)
+  - position: "relative" (by default)
