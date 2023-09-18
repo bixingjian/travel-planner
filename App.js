87b868f73cb06db2d6b2af68b3346c5957f1f33e
bixingjian <bixingjian19@163.com>
2023-09-18 15:56:14 -0400

    add fundamental concepts, like text button...

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  SafeAreaView,
+  Alert,
+  Button,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}>Hello World!</Text>
+      <TouchableOpacity onPress={() => console.log("image tapped")}>
+        <Image
+          blurRadius={7}
+          style={styles.stretch}
+          source={require("./assets/adaptive-icon.png")}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => console.log("image tapped")}
+        style={{ backgroundColor: "orange" }}
+      >
+        <Button
+          title="Click Me"
+          onPress={() =>
+            // Alert.alert("My Title", "My Message", [
+            //   { text: "Yes", onPress: () => console.log("Yes") },
+            //   { text: "No", onPress: () => console.log("No") },
+            // ])
+            Alert.prompt("My Title", "My Message", (text) => console.log(text))
+          }
+        />
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  stretch: {
+    width: 200,
+    height: 200,
+    resizeMode: "stretch",
   },
 });
