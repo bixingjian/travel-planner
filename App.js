516b1eb7540fe39931478d36e10b9edc7fa750be
bixingjian <bixingjian19@163.com>
2023-09-18 20:07:23 -0400

    finish view image screen component

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ import {
   useWindowDimensions,
 } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
